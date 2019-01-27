75e0de12b883427238ff6187a872c27ba82cd693
Henrik Salmela <henrik.salmela@gmail.com>
2019-01-27 23:16:39 +0200

    Build tools added

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import AcceptInvitation from './scenes/AcceptInvitationScene';
+import ForgotScene from './scenes/ForgotScene';
+import LoginScene from './scenes/LoginScene';
+import ResetPasswordScene from './scenes/ResetPasswordScene';
+
+export default {
+  AcceptInvitation,
+  ForgotScene,
+  LoginScene,
+  ResetPasswordScene,
+};
