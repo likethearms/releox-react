251d46bbf63cc9e045883996aa06ed2d1c88afa4
Henrik Salmela <henrik.salmela@gmail.com>
2019-01-29 18:06:38 +0200

    Scene props changed to optional

--- index.d.ts
+++ index.d.ts
@@ -4,64 +4,64 @@ interface IAuthRoutes {
 }
 
 interface IAcceptInvitationProps {
-  redirectUrl: string;
-  skipValidation: boolean;
-  title: string;
-  subTitle: string;
-  placeholder: string;
-  buttonText: string;
-  validateTokenUrl: string;
-  updateUrl: string;
-  removeAccessTokenUrl: string;
+  redirectUrl?: string;
+  skipValidation?: boolean;
+  title?: string;
+  subTitle?: string;
+  placeholder?: string;
+  buttonText?: string;
+  validateTokenUrl?: string;
+  updateUrl?: string;
+  removeAccessTokenUrl?: string;
 }
 
 interface IForgotSceneProps {
-  passwordResetAPIUrl: string;
-  backUrl: string;
-  emailRedirectUrl: string;
-  buttonText: string;
-  backText: string;
-  title: string;
-  subTitle: string;
-  placeholder: string;
-  thankYouTitle: string;
-  thankYouSubTitle: string;
-  thankYouBackText: string;
-  thankYouLink: string;
+  passwordResetAPIUrl?: string;
+  backUrl?: string;
+  emailRedirectUrl?: string;
+  buttonText?: string;
+  backText?: string;
+  title?: string;
+  subTitle?: string;
+  placeholder?: string;
+  thankYouTitle?: string;
+  thankYouSubTitle?: string;
+  thankYouBackText?: string;
+  thankYouLink?: string;
 }
 
 interface ILoginSceneProps {
-  title: string;
-  subTitle: string;
-  emailPlaceholder: string;
-  passwordPlaceholder: string;
-  forgotPasswordText: string;
-  loginButtonText: string;
-  registerText: string;
-  registerUrl: string;
-  redirectUrl: string;
-  forgotPasswordUrl: string;
-  preSetEmail: string;
-  preSetPassword: string;
-  loginRequestUrl: string;
-  tokenLocalStorageKey: string;
+  title?: string;
+  subTitle?: string;
+  emailPlaceholder?: string;
+  passwordPlaceholder?: string;
+  forgotPasswordText?: string;
+  loginButtonText?: string;
+  registerText?: string;
+  registerUrl?: string;
+  redirectUrl?: string;
+  forgotPasswordUrl?: string;
+  preSetEmail?: string;
+  preSetPassword?: string;
+  loginRequestUrl?: string;
+  tokenLocalStorageKey?: string;
 }
 
 interface IResetPasswordSceneProps {
-  title: string;
-  subTitle: string;
-  emailPlaceholder: string;
-  passwordPlaceholder: string;
-  forgotPasswordText: string;
-  loginButtonText: string;
-  registerText: string;
-  registerUrl: string;
-  redirectUrl: string;
-  forgotPasswordUrl: string;
-  preSetEmail: string;
-  preSetPassword: string;
-  loginRequestUrl: string;
-  tokenLocalStorageKey: string;
+  title?: string;
+  subTitle?: string;
+  emailPlaceholder?: string;
+  passwordPlaceholder?: string;
+  forgotPasswordText?: string;
+  loginButtonText?: string;
+  registerText?: string;
+  registerUrl?: string;
+  redirectUrl?: string;
+  forgotPasswordUrl?: string;
+  preSetEmail?: string;
+  preSetPassword?: string;
+  loginRequestUrl?: string;
+  tokenLocalStorageKey?: string;
 }
 
 declare module 'releox-react' {
