8130d5150b265f9af2694400d4217cad2d7d4d90
Henrik Salmela <henrik.salmela@gmail.com>
2019-04-02 21:34:45 +0300

    Types

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,10 @@
 import React from 'react';
-import LoginSceneProps from './src/scenes/LoginScene/LoginScene';
 
-// declare module 'releox-react' {
+interface LoginSceneProps {
+  onSubmit?: (body: any) => void;
+  onError?: (err: Error) => void;
+  showRegisterLink?: boolean;
+  locale?: any;
+}
+
 export class LoginScene extends React.Component<LoginSceneProps, any> { }
-// }
