79b2b0b315074cc2d562f8225fba54ac16b86b12
Henrik Salmela <henrik.salmela@gmail.com>
2019-04-02 21:06:38 +0300

    typings fix

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,6 @@
 import React from 'react';
 import LoginSceneProps from './src/scenes/LoginScene/LoginScene';
 
-export declare class LoginScene extends React.Component<LoginSceneProps, any> {}
+declare module 'releox-react' {
+  export class LoginScene extends React.Component<LoginSceneProps, any> { }
+}
