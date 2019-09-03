eebc4e5c7ac60768497fcff8d400375e7cd7bd7e
Eetu Laaksonen <eetlaa@gmail.com>
2019-09-03 15:40:35 +0300

    forgotsuccessscene test

---
+++ src/scenes/ForgotSuccessScene/ForgotSuccessScene.test.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { shallow, ShallowWrapper } from 'enzyme';
+import ForgotSuccessScene from './ForgotSuccessScene';
+import { ReleoxLocale } from '../../typings';
+
+describe('UI tests', () => {
+  describe('Finnish translations', () => {
+    let wrapper: ShallowWrapper;
+    beforeAll(() => {
+      wrapper = shallow(<ForgotSuccessScene />);
+    });
+
+    it('should have finnish title', () => {
+      const word = wrapper.find('AuthLayout').prop('title');
+      expect(word).toBe('Salasanan nollauspyyntö lähetetty!');
+    });
+
+    it('should have finnish subtitle', () => {
+      const word = wrapper.find('AuthLayout').prop('subTitle');
+      expect(word).toBe('Salasanan nollauslinkki on lähetetty sähköpostiisi! Avaa sähköpostissa oleva linkki ja vaihda salasana.'); // tslint:disable-line:max-line-length
+    });
+  });
+
+  describe('English translations', () => {
+    let wrapper: ShallowWrapper;
+    beforeAll(() => {
+      wrapper = shallow(<ForgotSuccessScene locale={ReleoxLocale.EN} />);
+    });
+
+    it('should have english title', () => {
+      const word = wrapper.find('AuthLayout').prop('title');
+      expect(word).toBe('Reset email is sent!');
+    });
+
+    it('should have english subtitle', () => {
+      const word = wrapper.find('AuthLayout').prop('subTitle');
+      expect(word).toBe('Password reset link is sent to your email. Click link on your email and change password.');
+    });
+  });
+
+  describe('getLinks test', () => {
+    it('should have default links', () => {
+      const wrapper = shallow(<ForgotSuccessScene />);
+      const links = wrapper.find('AuthLayout').prop('links');
+      expect(links).toEqual([
+        {
+          to: '/login',
+          id: 'login-link',
+          text: 'Kirjautumissivulle',
+        }],
+      );
+    });
+
+    it('should have default links in english if locale is set to EN', () => {
+      const wrapper = shallow(<ForgotSuccessScene locale={ReleoxLocale.EN} />);
+      const links = wrapper.find('AuthLayout').prop('links');
+      expect(links).toEqual([
+        {
+          to: '/login',
+          id: 'login-link',
+          text: 'Back to login',
+        }],
+      );
+    });
+  });
+});
