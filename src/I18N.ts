262c6371677abf4796e428ea3e9b6ffe65b36c4a
Henrik Salmela <henrik.salmela@gmail.com>
2019-09-05 21:46:28 +0300

    I18N documentation

--- src/I18N.ts
+++ src/I18N.ts
@@ -11,9 +11,23 @@ const getLocale = (locale?: ReleoxLocale): ReleoxLocale => {
   return 'fi';
 };
 
+/**
+ * Translation
+ *
+ * Function return string from translation file
+ * @param key string - translation key
+ * @param locale ReleoxLocale - ReleoxLocale value
+ */
 export const t = (key: string, locale?: ReleoxLocale): string => {
   const l = getLocale(locale);
   return translations[l][key];
 };
 
+/**
+ * Translation currying version
+ * This version is cleaner to use in more complicated components
+ * or long components than regular t function
+ * @param prefix string
+ * @param locale string
+ */
 export const ct = (prefix: string, locale?: ReleoxLocale) => (key: string): string => t(`${prefix}.${key}`, locale);
