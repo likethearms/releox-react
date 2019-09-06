3be790a6f668a8a89aa0ff590a3dae38d9ccbf84
Henrik Salmela <henrik.salmela@gmail.com>
2019-09-06 22:58:24 +0300

    Fixing some code issues & Cognitive Complexity

--- src/parse-params.ts
+++ src/parse-params.ts
@@ -5,18 +5,19 @@ interface ParseParams {
   access_token: string; // eslint-disable-line camelcase
 }
 
-export const parseParams = (
-  strict: boolean = false,
-): Promise<ParseParams> => new Promise((resolve, reject) => {
+export const parseParams = (strict: boolean = false) => new Promise((resolve, reject) => {
   const query = queryString.parse(window.location.search);
+
   const params = {
     access_token: query.access_token as string,
     user: query.user as string,
   };
+
   if (strict) {
     if (!params.user || !params.access_token) {
       return reject(new Error('Missing User Object or Access Token'));
     }
   }
+
   return resolve(params);
 });
