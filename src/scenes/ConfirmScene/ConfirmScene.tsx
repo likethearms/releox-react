16378867d076f6c62b3eda6ff416368107848ec4
Eetu Laaksonen <eetlaa@gmail.com>
2019-09-06 19:26:55 +0300

    comfirmscene test

--- src/scenes/ConfirmScene/ConfirmScene.tsx
+++ src/scenes/ConfirmScene/ConfirmScene.tsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import queryString from 'query-string';
 import Axios, { AxiosError } from 'axios';
 import { Redirect } from 'react-router';
-import { successScene } from '../../HOC/success-scene';
+import { successScene, SuccessSceneProps } from '../../HOC/success-scene';
 import { routes } from '../../routes';
 import { apis } from '../../apis';
 import { getErrorMessage } from '../../config';
@@ -14,7 +14,7 @@ interface State {
   loading: boolean;
 }
 
-export class ConfirmScene extends Component<ConfirmSceneProps, State> {
+export class ConfirmScene extends Component<ConfirmSceneProps & SuccessSceneProps, State> {
   constructor(props: ConfirmSceneProps) {
     super(props);
     this.state = {
@@ -33,9 +33,10 @@ export class ConfirmScene extends Component<ConfirmSceneProps, State> {
   }
 
   render(): JSX.Element {
+    const { locale } = this.props;
     const { redirect, loading } = this.state;
     if (redirect) return <Redirect to={redirect} />;
     if (loading) return <Loading centeredVertical />;
-    return successScene('ConfirmScene', 'confirm', routes.LOGIN)({});
+    return successScene('ConfirmScene', 'confirm', routes.LOGIN)({ locale });
   }
 }
