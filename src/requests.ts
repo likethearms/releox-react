fadad2d3b9c744f37320399cbc4d43aaafb9d9c4
Henrik Salmela <henrik.salmela@gmail.com>
2019-09-07 18:08:40 +0300

    Fixing dublication

--- src/requests.ts
+++ src/requests.ts
@@ -1,11 +1,11 @@
 import Axios from 'axios';
 import { apis } from './apis';
 
-export const validateTokenRequest = (accessToken: string, user: string) => Axios
-  .get(`${apis.MEMBER}/${user}?access_token=${accessToken}`);
+const get = (url: string) => Axios.get(url);
 
-export const confirmUserRequest = (userId: string, confirmationToken: string) => Axios
-  .get(`${apis.CONFIRM}?uid=${userId}&token=${confirmationToken}`);
+export const validateTokenRequest = (accessToken: string, user: string) => get(`${apis.MEMBER}/${user}?access_token=${accessToken}`);
+export const validateInvitationTokenRequest = (userId: string, invitationToken: string) => get(`${apis.VALIDATE_INVITATION_TOKEN}?uid=${userId}&invitation_token=${invitationToken}`);
+export const confirmUserRequest = (userId: string, confirmationToken: string) => get(`${apis.CONFIRM}?uid=${userId}&token=${confirmationToken}`);
 
 export const patchUserRequest = (userId: string, body: any) => Axios
   .patch(`${apis.PATCH}/${userId}`, body);
@@ -15,6 +15,3 @@ export const logoutRequest = (accessToken: string) => Axios
 
 export const loginRequest = (body: any) => Axios
   .post(apis.LOGIN, body);
-
-export const validateInvitationTokenRequest = (userId: string, invitationToken: string) => Axios
-  .get(`${apis.VALIDATE_INVITATION_TOKEN}?uid=${userId}&invitation_token=${invitationToken}`);
