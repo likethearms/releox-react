05e4124382321085793946fda7b79f463d2477b7
Henrik Salmela <henrik.salmela@gmail.com>
2019-09-07 13:42:45 +0300

    CardTitle tests added

--- src/components/CardTitle/CardTitle.test.tsx
+++ src/components/CardTitle/CardTitle.test.tsx
@@ -2,7 +2,17 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { CardTitle } from './CardTitle';
 
-it('should show input', () => {
+it('should show h5', () => {
   const wrapper = shallow(<CardTitle>Foo</CardTitle>);
-  expect(wrapper.find({ children: 'Foo' }).length).toBe(1);
+  expect(wrapper.find('h5[children="Foo"]')).toHaveLength(1);
+});
+
+it('should show h1', () => {
+  const wrapper = shallow(<CardTitle xl>Foo</CardTitle>);
+  expect(wrapper.find('h1[children="Foo"]')).toHaveLength(1);
+});
+
+it('should show h3', () => {
+  const wrapper = shallow(<CardTitle lg>Foo</CardTitle>);
+  expect(wrapper.find('h3[children="Foo"]')).toHaveLength(1);
 });
