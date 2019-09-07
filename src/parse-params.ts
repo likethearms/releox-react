4fdeaa6dfd2904de08e5816420659fb7816c689d
Henrik Salmela <henrik.salmela@gmail.com>
2019-09-07 17:35:48 +0300

    parse params refactory

--- src/parse-params.ts
+++ src/parse-params.ts
@@ -5,21 +5,21 @@ interface ParseParams {
   access_token: string; // eslint-disable-line camelcase
 }
 
-export const parseParams = (
-  strict: boolean = false,
-): Promise<ParseParams> => new Promise((resolve, reject) => {
+export const parseParams = (strict: boolean = false): Promise<ParseParams> => {
   const query = queryString.parse(window.location.search);
 
-  const params = {
-    access_token: query.access_token as string,
-    user: query.user as string,
-  };
+  return new Promise((resolve, reject) => {
+    const params = {
+      access_token: query.access_token as string,
+      user: query.user as string,
+    };
 
-  if (strict) {
-    if (!params.user || !params.access_token) {
-      return reject(new Error('Missing User Object or Access Token'));
+    if (strict) {
+      if (!params.user || !params.access_token) {
+        return reject(new Error('Missing User Object or Access Token'));
+      }
     }
-  }
 
-  return resolve(params);
-});
+    return resolve(params);
+  });
+};
