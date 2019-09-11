351ec69e1acacf5d370fb5bf9589be44dd00abfb
Henrik Salmela <henrik.salmela@gmail.com>
2019-09-11 22:40:48 +0300

    AsyncSelectFormikWrapper test descriptions

---
+++ src/components/AsyncSelect/AsyncSelectFormikWrapper.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+it('should inject value', () => { });
+it('should inject getUrl', () => { });
+it('should inject onChange', () => { });
+it('should inject queryFormat', () => { });
+it('should inject placeholder', () => { });
+it('should inject ["name"] searchFields by default', () => { });
+it('should inject ["foo"] if searchFields is given', () => { });
