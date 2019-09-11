9b8fd059d87ea98699e73eeca84e1a7163ce6742
Henrik Salmela <henrik.salmela@gmail.com>
2019-09-11 19:00:15 +0300

    AbstractFormikInputWrapper test

--- src/components/AbstractFormikInputWrapper/AbstractFormikInputWrapper.test.tsx
+++ src/components/AbstractFormikInputWrapper/AbstractFormikInputWrapper.test.tsx
@@ -1,11 +1,13 @@
 import { AbstractFormikInputWrapper, AbstractFormikInputWrapperProps } from './AbstractFormikInputWrapper';
 
-class A extends AbstractFormikInputWrapper<any, AbstractFormikInputWrapperProps> { }
+class A extends AbstractFormikInputWrapper<string, AbstractFormikInputWrapperProps> { }
 
 describe('AbstractFormikInputWrapper', () => {
-  const inputWrapper = new A({ form: { setFieldValue: () => { } }, field: { name: 'foo' } });
+  const spy = jest.fn();
+  const inputWrapper = new A({ form: { setFieldValue: spy }, field: { name: 'foo' } });
 
   it('', () => {
-    console.log(inputWrapper);
+    inputWrapper.onChange('Bar');
+    expect(spy).toBeCalledWith('foo', 'Bar');
   });
 });
