1e4c8018a22fd7344991cf33be46b3f9418884ae
Henrik Salmela <henrik.salmela@gmail.com>
2019-09-11 22:56:43 +0300

    AsyncSelectFormikWrapper test

--- src/components/AsyncSelect/AsyncSelectFormikWrapper.test.tsx
+++ src/components/AsyncSelect/AsyncSelectFormikWrapper.test.tsx
@@ -1,9 +1,58 @@
 import React from 'react';
+import { shallow } from 'enzyme';
+import { AsyncSelectFormikWrapper } from './AsyncSelectFormikWrapper';
 
-it('should inject value', () => { });
-it('should inject getUrl', () => { });
-it('should inject onChange', () => { });
-it('should inject queryFormat', () => { });
-it('should inject placeholder', () => { });
-it('should inject ["name"] searchFields by default', () => { });
-it('should inject ["foo"] if searchFields is given', () => { });
+describe('AsyncSelectFormikWrapper', () => {
+  const setFieldValue = jest.fn();
+  const wrapper = shallow((
+    <AsyncSelectFormikWrapper
+      getUrl="fooUrl"
+      field={{ value: 'bar', name: 'fieldName' }}
+      queryFormat="mongodb"
+      form={{ setFieldValue } as any}
+      placeholder="fooPlaceholder"
+    />
+  ));
+  const select = wrapper.find('AsyncSelect');
+
+  it('should inject value', () => {
+    expect(select.prop('value')).toBe('bar');
+  });
+
+  it('should inject getUrl', () => {
+    expect(select.prop('getUrl')).toBe('fooUrl');
+  });
+
+  it('should inject onChange', () => {
+    const onChange = select.prop('onChange') as Function;
+    onChange('Bar');
+    expect(setFieldValue).toBeCalledWith('fieldName', 'Bar');
+  });
+
+  it('should inject queryFormat', () => {
+    expect(select.prop('queryFormat')).toBe('mongodb');
+  });
+
+  it('should inject placeholder', () => {
+    expect(select.prop('placeholder')).toBe('fooPlaceholder');
+  });
+
+  it('should inject ["name"] searchFields by default', () => {
+    expect(select.prop('searchFields')).toStrictEqual(['name']);
+  });
+
+  it('should inject ["foo"] if searchFields is given', () => {
+    const w = shallow((
+      <AsyncSelectFormikWrapper
+        getUrl="fooUrl"
+        field={{ value: 'bar', name: 'fieldName' }}
+        queryFormat="mongodb"
+        form={{ setFieldValue } as any}
+        placeholder="fooPlaceholder"
+        searchFields={['foo']}
+      />
+    ));
+    const s = w.find('AsyncSelect');
+    expect(s.prop('searchFields')).toStrictEqual(['foo']);
+  });
+});
