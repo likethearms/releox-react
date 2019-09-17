3623c5d44cce94e1dba0a20f5d4edb75d63c91fa
Henrik Salmela <henrik.salmela@gmail.com>
2019-09-17 16:27:57 +0300

    Checkbox onChange

--- src/components/Checkbox/CheckBox.tsx
+++ src/components/Checkbox/CheckBox.tsx
@@ -4,14 +4,30 @@ import { Field } from 'formik';
 export interface CheckBoxProps {
   id?: string;
   className?: string;
+  onChange?: Function;
   name: string;
 }
 
-export const CheckBox = ({ id, className, name }: CheckBoxProps) => (
-  <Field
-    name={name}
-    id={id || `${name}-input`}
-    className={className || 'form-control'}
-    render={({ field }: any) => (<input type="checkbox" checked={field.value} {...field} />)} /* eslint-disable-line react/jsx-props-no-spreading */
-  />
-);
+export const CheckBox = (props: CheckBoxProps) => {
+  const {
+    id, className, name, onChange,
+  } = props;
+  return (
+    <Field
+      name={name}
+      id={id || `${name}-input`}
+      className={className || 'form-control'}
+      render={({ field }: any) => (
+        <input
+          type="checkbox"
+          checked={field.value}
+          {...field} /* eslint-disable-line react/jsx-props-no-spreading */
+          onChange={(e) => {
+            if (onChange) onChange(e);
+            field.onChange(e);
+          }}
+        />
+      )}
+    />
+  );
+};
