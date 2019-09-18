e910e659d3faaccb034d6a01262fc0d140eeef1a
Eetu Laaksonen <eetlaa@gmail.com>
2019-09-18 19:27:25 +0300

    input inline

---
+++ src/testing-cra/Components.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Card } from '../components/Card/Card';
+import { FormikFormWrapper } from '../components/FormikFormWrapper/FormikFormWrapper';
+import { AsyncSelectFormik } from '../components/AsyncSelect/AsyncSelectFormik';
+import { CoreuiCheckBox } from '../components/Checkbox/CoreuiCheckBox';
+import { Input } from '../components/Input/Input';
+
+const Components = () => (
+  <div className="m-5">
+    <Card>
+      <FormikFormWrapper<any>
+        onSubmit={() => { }}
+        initialValues={{ name: '' }}
+      >
+        <AsyncSelectFormik
+          getUrl="/Products/"
+          queryFormat="mongodb"
+          label="Products"
+          name="products"
+        />
+        <Input inline name="hello" label="Hello" />
+        <Input labelClass="text-right" inline name="hello" label="Hello" />
+        <Input inline name="hello" label="Hello" />
+        <Input labelClass="text-right" inlineLabelWidth={2} inline name="hello" label="Hello" />
+        <Input inlineLabelWidth={10} inline name="hello" label="Hello" />
+        <CoreuiCheckBox label="Admin" name="admin" />
+        <CoreuiCheckBox label="Label" name="Label" />
+      </FormikFormWrapper>
+    </Card>
+  </div>
+);
+
+export default Components;
