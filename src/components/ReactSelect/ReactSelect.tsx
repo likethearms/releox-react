9c50f977ae4225755e43652482a03df93e5f1cce
Henrik Salmela <henrik.salmela@gmail.com>
2019-10-04 00:12:12 +0300

    small improvements

--- src/components/ReactSelect/ReactSelect.tsx
+++ src/components/ReactSelect/ReactSelect.tsx
@@ -19,17 +19,16 @@ export class ReactSelect extends AbstractFormikInputGroup<SelectProps> {
   getInputElement = (fieldProps: FieldProps) => {
     const { placeholder, label, name, id, options } = this.props;
     return (
-      <div>
+      <div className="ReactSelectHelper">
         <Select
           {...fieldProps.field} // eslint-disable-line
           id={id || `${name}-input`}
           options={options}
+          classNamePrefix="ReactSelect"
           placeholder={placeholder || label}
           className={`${this.getInvalidClass(fieldProps)}`}
           value={options.find((option: Option) => option.value === fieldProps.field.value)}
-          onChange={(option: any) =>
-            fieldProps.form.setFieldValue(fieldProps.field.name, option.value)
-          } // eslint-disable-line
+          onChange={(option: any) => fieldProps.form.setFieldValue(name, option.value)}
         />
         {this.getErrorMessageField()}
       </div>
--- src/testing-cra/Components.tsx
+++ src/testing-cra/Components.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import { Formik, Form } from 'formik';
 import { Card } from '../components/Card/Card';
-import { AsyncSelectFormik } from '../components/AsyncSelect/AsyncSelectFormik';
-import { TextArea } from '../components/TextArea/TextArea';
-import { Input } from '../components/Input/Input';
 import { ReactSelect } from '../components/ReactSelect/ReactSelect';
 
 const Components = () => (
@@ -13,30 +10,18 @@ const Components = () => (
       <Formik
         onSubmit={(x) => x}
         initialValues={{
-          name: '',
-          foo: '',
-          async: '',
-          reactSelect: '',
+          reactSelect: 'foo',
+          selectReact: '',
         }}
         validate={(v) => {
           const e: any = {};
-          if (!v.name) e.name = 'Missing';
+          if (!v.selectReact) e.selectReact = 'Missing';
           return e;
         }}
       >
         {() => (
           <Form>
             <button type="submit">Submit</button>
-            <AsyncSelectFormik
-              getUrl="http://localhost:3001/api/Vehicles"
-              queryFormat="mongodb"
-              label="Products"
-              mapLabel="registerNumber"
-              searchFields={['registerNumber']}
-              name="async"
-            />
-            <TextArea name="name" label="Name" />
-            <Input name="foo" label="foo" />
             <ReactSelect
               options={[{ label: 'Foo', value: 'foo' }, { label: 'Bar', value: 'bar' }]}
               label="ReactSelect"
