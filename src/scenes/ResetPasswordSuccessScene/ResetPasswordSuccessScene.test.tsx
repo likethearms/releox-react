0a5dd91c4af0b7590bab3ef273f2f84d609ec6ef
Essi Kivivuori <essi.kivivuori@gmail.com>
2019-12-18 16:13:20 +0200

    Add ResetPasswordSuccessScene tests

--- src/scenes/ResetPasswordSuccessScene/ResetPasswordSuccessScene.test.tsx
+++ src/scenes/ResetPasswordSuccessScene/ResetPasswordSuccessScene.test.tsx
@@ -1,69 +1,31 @@
 import React from 'react';
-import { shallow, ShallowWrapper } from 'enzyme';
+import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router';
 import { ResetPasswordSuccessScene } from './ResetPasswordSuccessScene';
 
-describe('UI tests', () => {
-  describe('Finnish translations', () => {
-    let wrapper: ShallowWrapper;
-    beforeAll(() => {
-      wrapper = shallow(<ResetPasswordSuccessScene />);
+describe('ResetPasswordSuccessScene', () => {
+  describe('UI Elements', () => {
+    it('should have title', () => {
+      const { debug, getByText } = render(<ResetPasswordSuccessScene />, { wrapper: MemoryRouter });
+      debug();
+      expect(getByText('Your password is changed')).toBeTruthy();
     });
 
-    it('should have finnish title', () => {
-      const word = wrapper.find('AuthLayout').prop('title');
-      expect(word).toBe('Salasanasi on vaihdettu');
+    it('should have subtitle', () => {
+      const { getByText } = render(<ResetPasswordSuccessScene />, { wrapper: MemoryRouter });
+      expect(
+        getByText(
+          'Your password is now changed. Go back to login page and log in with new password.'
+        )
+      ).toBeTruthy();
     });
 
-    it('should have finnish subtitle', () => {
-      const word = wrapper.find('AuthLayout').prop('subTitle');
-      expect(word).toBe(
-        'Salasanasi on vaihdettu onnistuneesti. Voit nyt palata kirjautumissivulle ja kirjautua uudella salasanalla'
-      );
-    });
-  });
-
-  describe('English translations', () => {
-    let wrapper: ShallowWrapper;
-    beforeAll(() => {
-      wrapper = shallow(<ResetPasswordSuccessScene locale="en" />);
-    });
-
-    it('should have english title', () => {
-      const word = wrapper.find('AuthLayout').prop('title');
-      expect(word).toBe('Your password is changed');
-    });
-
-    it('should have english subtitle', () => {
-      const word = wrapper.find('AuthLayout').prop('subTitle');
-      expect(word).toBe(
-        'Your password is now changed. Go back to login page and log in with new password.'
-      );
-    });
-  });
-
-  describe('getLinks test', () => {
-    it('should have default links', () => {
-      const wrapper = shallow(<ResetPasswordSuccessScene />);
-      const links = wrapper.find('AuthLayout').prop('links');
-      expect(links).toEqual([
-        {
-          to: '/login',
-          id: 'login-link',
-          text: 'Kirjautumissivulle',
-        },
-      ]);
-    });
-
-    it('should have default links in english if locale is set to EN', () => {
-      const wrapper = shallow(<ResetPasswordSuccessScene locale="en" />);
-      const links = wrapper.find('AuthLayout').prop('links');
-      expect(links).toEqual([
-        {
-          to: '/login',
-          id: 'login-link',
-          text: 'To login',
-        },
-      ]);
+    it('should have back to login link', () => {
+      const { container } = render(<ResetPasswordSuccessScene />, { wrapper: MemoryRouter });
+      const links = container.getElementsByTagName('a');
+      expect(links).toHaveLength(1);
+      expect(links[0].innerHTML).toBe('To login');
+      expect(links[0].getAttribute('href')).toBe('/login');
     });
   });
 });
