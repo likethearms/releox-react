2b1408381e042379964f134d0ecf2c0623b07a6f
Essi Kivivuori <essi.kivivuori@gmail.com>
2019-12-18 15:40:01 +0200

    Add ForgotSuccessScene tests

--- src/scenes/ForgotSuccessScene/ForgotSuccessScene.test.tsx
+++ src/scenes/ForgotSuccessScene/ForgotSuccessScene.test.tsx
@@ -1,69 +1,30 @@
 import React from 'react';
-import { shallow, ShallowWrapper } from 'enzyme';
+import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router';
 import { ForgotSuccessScene } from './ForgotSuccessScene';
 
-let wrapper: ShallowWrapper;
-
-describe('UI tests', () => {
-  describe('Finnish translations', () => {
-    beforeAll(() => {
-      wrapper = shallow(<ForgotSuccessScene />);
+describe('ForgotSuccessScene', () => {
+  describe('UI Elements', () => {
+    it('should have a title message', () => {
+      const { getByText } = render(<ForgotSuccessScene />, { wrapper: MemoryRouter });
+      expect(getByText('Reset email is sent!')).toBeTruthy();
     });
 
-    it('should have finnish title', () => {
-      const word = wrapper.find('AuthLayout').prop('title');
-      expect(word).toBe('Salasanan nollauspyyntö lähetetty!');
+    it('should have a text muted message', () => {
+      const { getByText } = render(<ForgotSuccessScene />, { wrapper: MemoryRouter });
+      expect(
+        getByText(
+          'Password reset link is sent to your email. Click link on your email and change password.'
+        )
+      ).toBeTruthy();
     });
 
-    it('should have finnish subtitle', () => {
-      const word = wrapper.find('AuthLayout').prop('subTitle');
-      expect(word).toBe(
-        'Salasanan nollauslinkki on lähetetty sähköpostiisi! Avaa sähköpostissa oleva linkki ja vaihda salasana.'
-      );
-    });
-  });
-
-  describe('English translations', () => {
-    beforeAll(() => {
-      wrapper = shallow(<ForgotSuccessScene locale="en" />);
-    });
-
-    it('should have english title', () => {
-      const word = wrapper.find('AuthLayout').prop('title');
-      expect(word).toBe('Reset email is sent!');
-    });
-
-    it('should have english subtitle', () => {
-      const word = wrapper.find('AuthLayout').prop('subTitle');
-      expect(word).toBe(
-        'Password reset link is sent to your email. Click link on your email and change password.'
-      );
-    });
-  });
-
-  describe('getLinks test', () => {
-    it('should have default links', () => {
-      wrapper = shallow(<ForgotSuccessScene />);
-      const links = wrapper.find('AuthLayout').prop('links');
-      expect(links).toEqual([
-        {
-          to: '/login',
-          id: 'login-link',
-          text: 'Takaisin kirjautumissivulle',
-        },
-      ]);
-    });
-
-    it('should have default links in english if locale is set to EN', () => {
-      wrapper = shallow(<ForgotSuccessScene locale="en" />);
-      const links = wrapper.find('AuthLayout').prop('links');
-      expect(links).toEqual([
-        {
-          to: '/login',
-          id: 'login-link',
-          text: 'Back to login',
-        },
-      ]);
+    it('should have back to login link', () => {
+      const { container } = render(<ForgotSuccessScene />, { wrapper: MemoryRouter });
+      const links = container.getElementsByTagName('a');
+      expect(links).toHaveLength(1);
+      expect(links[0].innerHTML).toBe('Back to login');
+      expect(links[0].getAttribute('href')).toBe('/login');
     });
   });
 });
